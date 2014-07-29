5a79d8ec36871d8897b230dd7d3a5c804acaa1bb
jen2014 <windbreiz@gmail.com>
2014-07-29 10:28:54 -0400

    New working rss link
    Previous link generated lastBuildDate error in RSS::Parser.parse()

---
+++ rss01_activity.rb
@@ -0,0 +1,32 @@
+require 'ruboto/widget'
+require 'ruboto/util/toast'
+require 'rss'
+ruboto_import_widgets :Button, :LinearLayout, :TextView, :ListView
+
+$subjects = []
+url = 'http://www.nasa.gov/rss/dyn/breaking_news.rss'
+#
+# Collect list of items from RSS xml
+# ----------------------------------
+rss = RSS::Parser.parse(url)
+rss.items.each do |item|	
+	subject = item.title.to_s
+	$subjects << subject
+end
+#
+# Display list of items
+# ---------------------
+class Rss0Activity
+	def onCreate(bundle)
+    super
+    set_title 'Download rss topics'
+    self.content_view =
+        linear_layout :orientation => :vertical do
+          @list_view = list_view :list => []
+        end
+  end
+	def onResume
+    super
+    @list_view.adapter.add_all $subjects
+  end
+end
